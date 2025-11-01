2f9d3c2e0fc7492dbf78474a2f17cbdf8efee2ae
Naila Syarifa Yosarvi <naila.syarifa@ui.ac.id>
2025-11-02 01:12:39 +0700

    TI: Initialize project

---
+++ src/test/java/apap/ti/_5/vehicle_rental_2306245882_be/VehicleRental2306245882BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.vehicle_rental_2306245882_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRental2306245882BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
